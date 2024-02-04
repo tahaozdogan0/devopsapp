e3f4f3733319279e34f4f5653aad3db9dcf5e6d5
Taha Kubilay ÖZDOĞAN <tahaozdogan79@gmail.com>
2024-02-04 19:52:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.listen(port, () => {
 });
 
 const connection = mysql.createConnection({
-  host: '10.182.0.4:3306',
+  host: 'instance-1:3306',
   user: 'root',
   database: 'devopsapp',
   password: '644529taha'
